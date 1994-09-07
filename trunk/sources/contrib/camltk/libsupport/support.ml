84e89cbb420dc7d4375a3eafe1682a3cb2a089a1
rouaix <rouaix@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1994-09-07 18:50:39 +0000

    Added: retypage des widgets retournes

--- trunk/sources/contrib/camltk/libsupport/support.ml
+++ trunk/sources/contrib/camltk/libsupport/support.ml
@@ -18,39 +18,71 @@ let default_toplevel_widget =  ("toplevel", "." : Widget)
 let CAMLtoTKWidget _ = widget_name
 ;;
 
-(* TODO We should restore the proper type *)
-let TKtoCAMLWidget s =
-  (s,"unknown")
-;;
-
-let Widget_any_table = 
-  ["button"; "checkbutton"; "entry"; "listbox"; "canvas"; 
-    "menu"; "menubutton"; "radiobutton"; "scale"; "scrollbar"; 
-    "text"; "toplevel" ]
-;;
-let Widget_menu_table = [ "menu" ]
-;;
-let Widget_frame_table = [ "frame" ]
-;;
-
-let widget_naming_scheme =
-  ref [ "button", "b";
+let widget_naming_scheme = [
+        "button", "b";
+	"canvas", "ca";
 	"checkbutton", "cb";
 	"entry", "en";
+        "frame", "f";
+	"label", "l";
 	"listbox", "li";
-	"canvas", "ca";
-	"menu", "m";
+	"menu", "me";
 	"menubutton", "mb";
+	"message", "ms";
         "radiobutton", "rb";
 	"scale", "sc";
 	"scrollbar", "sb";
 	"text", "t";
       	"toplevel", "T" ]
 ;;
+let widget_typing_scheme = 
+   map (fun (x,y) -> (y,x)) widget_naming_scheme
+;;
+
+let is_digit = function
+   `0`..`9` -> true
+ | _ -> false
+;;
+
+let is_point c = 
+   c = `.`
+;;
+
+let type_of_widget path =
+  let rec lastchar n =
+    if is_digit (nth_char path n) then
+      lastchar (pred n)
+    else n
+  and firstchar n =
+    if is_point (nth_char path n) then succ n
+    else firstchar (pred n) 
+  in
+  let l = lastchar (string_length path - 1) in
+  let f = firstchar l in
+   try
+     assoc (sub_string path f (l-f+1)) widget_typing_scheme
+   with
+     Not_found -> raise (Invalid_argument ("invalid widget path :"^path))
+;;
+
+(* Retype widgets returned from Tk *)
+let TKtoCAMLWidget s =
+  (type_of_widget s,s)
+;;
+
+
+let Widget_any_table =  map fst widget_naming_scheme
+;;
+(* subtypes at this time *)
+let Widget_menu_table = [ "menu" ]
+;;
+let Widget_frame_table = [ "frame" ]
+;;
+
 
 let new_suffix class n =
   try 
-    (assoc class !widget_naming_scheme) ^ (string_of_int n)
+    (assoc class widget_naming_scheme) ^ (string_of_int n)
   with
     Not_found -> "w" ^ (string_of_int n)
 ;;
@@ -68,7 +100,7 @@ let new_widget_atom =
 ;;
 
 (* Redundant with subtyping of Widget *)
-
+(* but used to check types *)
 let check_widget_class w class =
   if widget_class w = class then ()
   else raise (IllegalWidgetType (widget_class w))
