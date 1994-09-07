99c0b8e54453ccbc4fbee77f56deecf34226b19a
rouaix <francois.rouaix@inria.fr>
1994-09-07 18:48:54 +0000

    Added: recuperation messages d'erreur Removed: include_file

--- sources/contrib/camltk/compiler/maincompile.ml
+++ sources/contrib/camltk/compiler/maincompile.ml
@@ -6,8 +6,6 @@
 
 let input_name = ref "Widgets.src"
 ;;
-let include_file = ref "builtins"
-;;
 
 let usage () = 
   prerr_string "Usage: tkcompiler input.src\n";
@@ -66,9 +64,11 @@ let compile () =
 		  write_TKtoCAML (output_string oc) typname typdef
       	      with 
       	       Not_found -> 
-      	       	prerr_string "Type ";
-      	       	prerr_string typname;
-      	       	prerr_string " is external or undefined\n")
+	        if not (mem typname !types_external) then begin
+       	       	  prerr_string "Type ";
+      	       	  prerr_string typname;
+      	       	  prerr_string " is undeclared external or undefined\n"
+                end)
       	    sorted_types;
     do_list (write_function (output_string oc)) !function_table;
     close_out oc;
@@ -81,7 +81,7 @@ let compile () =
 	begin match wdef.ModuleType with
 	  Widget ->
             write_create (output_string oc) wname;
-      	    do_list (write_command (output_string oc)) wdef.Commands
+      	    do_list (write_command wname (output_string oc)) wdef.Commands
         | Family ->
 	    do_list (write_function (output_string oc)) wdef.Commands
         end;
@@ -101,13 +101,31 @@ let compile () =
 ;;
 
 let main () =
-  arg__parse ["-i", arg__String (fun s -> include_file := s)]
+  arg__parse []
       	     (fun filename ->
 	        input_name := filename)
       	      ;
-  parse_file !input_name;
-  compile ();
-  exit 0
+  try 
+    parse_file !input_name;
+    compile ();
+    exit 0
+  with
+    Lexical_error s ->
+      prerr_string "Invalid lexical character: ";
+      prerr_endline s;
+      exit 1
+ | Duplicate_Definition (s,s') ->
+      prerr_string s; prerr_string " "; prerr_string s';
+      prerr_endline " is redefined illegally";
+      exit 1
+ | Invalid_implicit_constructor c ->
+      prerr_string "Constructor ";
+      prerr_string c;
+      prerr_endline " is used implicitly before defined";
+      exit 1
+ | Cyclic ->
+      prerr_endline "Cyclic dependency of types";
+      exit 1
 ;;
 
 printexc__f main ()
