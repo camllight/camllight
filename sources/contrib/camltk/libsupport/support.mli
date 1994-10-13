c892b82414ef375b04dc6d2d5c51d76f807000df
rouaix <francois.rouaix@inria.fr>
1994-10-13 10:19:27 +0000

    *** empty log message ***

--- sources/contrib/camltk/libsupport/support.mli
+++ sources/contrib/camltk/libsupport/support.mli
@@ -8,10 +8,15 @@ value default_toplevel_widget : Widget;;
 value new_toplevel_widget : string -> Widget;;
        (* the argument must be a valid window name: no 8bit char, start
           with a lowercase, ... *)
+
+(* The following functions are used internally.        *)
+(* There is normally no need for them in users programs *)
           
 value widget_name : Widget -> string;;
+
 value CAMLtoTKWidget : 'a -> Widget -> string;;
 value TKtoCAMLWidget : string -> Widget;;
+
 value Widget_any_table : string list;;
 value Widget_menu_table : string list;;
 value Widget_frame_table : string list;;
