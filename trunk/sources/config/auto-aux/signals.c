dd27371fa19ec7d713aa0460fdc7e09828280435
xleroy <xleroy@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1994-11-10 09:48:19 +0000

    Meilleur test pour getgroups. Ajout de tests pour libgraph. Detection des signaux revue pour Linux.

--- trunk/sources/config/auto-aux/signals.c
+++ trunk/sources/config/auto-aux/signals.c
@@ -5,6 +5,26 @@
 #include <stdio.h>
 #include <signal.h>
 
+/* Find a signal that is ignored by default */
+
+#ifdef SIGCHLD
+#define IGNSIG SIGCHLD
+#else
+#ifdef SIGIO
+#define IGNSIG SIGIO
+#else
+#ifdef SIGCLD
+#define IGNSIG SIGCLD
+#else
+#ifdef SIGPWR
+#define IGNSIG SIGPWR
+#endif
+#endif
+#endif
+#endif
+
+#ifdef IGNSIG
+
 int counter;
 
 void sig_handler(dummy)
@@ -17,9 +37,22 @@ int main(argc, argv)
      int argc;
      char ** argv;
 {
-  signal(SIGINT, sig_handler);
+  signal(IGNSIG, sig_handler);
   counter = 0;
-  kill(getpid(), SIGINT);
-  kill(getpid(), SIGINT);
+  kill(getpid(), IGNSIG);
+  kill(getpid(), IGNSIG);
   return (counter == 2 ? 0 : 1);
 }
+
+#else
+
+/* If no suitable signal was found, assume System V */
+
+int main(argc, argv)
+     int argc;
+     char ** argv;
+{
+  return 1;
+}
+
+#endif
