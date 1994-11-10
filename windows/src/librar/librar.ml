22933d3f9a5774eb41632921cca18a31e2e2e67d
xleroy <xavier.leroy@inria.fr>
1994-11-10 10:03:51 +0000

    Messages d'erreur.

--- windows/src/librar/librar.ml
+++ windows/src/librar/librar.ml
@@ -7,6 +7,7 @@ let offset_compiled_phrase ofs cp =
   { cph_pos = cp.cph_pos + ofs;
     cph_len = cp.cph_len;
     cph_reloc = cp.cph_reloc;
+    cph_events = cp.cph_events;
     cph_pure = cp.cph_pure }
 ;;
 
@@ -22,10 +23,9 @@ let add_to_library outchan (offset, index_rest) filename =
     close_in inchan;
     let new_index = map (offset_compiled_phrase offset) old_index in
       (offset + len, new_index @ index_rest)
-  with x ->
-    prerr_begline ">> Error on file ";
-    prerr_endline filename;
-    raise x
+  with Cannot_find_file name ->
+    printf__eprintf "Cannot find file %s.\n" name;
+    raise Toplevel
 ;;    
 
 let make_library file_list library_name =
