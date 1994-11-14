703784356b294eaf0060cc0ec1a6ebdc3b49e955
rouaix <francois.rouaix@inria.fr>
1994-11-14 13:24:41 +0000

    *** empty log message ***

--- sources/contrib/camltk/compiler/intf.ml
+++ sources/contrib/camltk/compiler/intf.ml
@@ -2,34 +2,43 @@
 #open "tables";;
 #open "compile";;
 
-let write_create_p W wname =
-  W "value create : Widget -> option list -> Widget ;;\n";
-  W "             (* [create parent options] creates a new widget. Options\n";
-  W "                are restricted to the widget class subset. *)\n"
+let write_create_p w wname =
+  w "value create : Widget -> option list -> Widget ;;\n";
+  w "             (* [create p options] creates a new widget with parent p.";
+  w "		     Options are restricted to the widget class subset,";
+  w "		     and checked dynamically. *)\n"
 ;;
 
-let write_command_p wname W def =
-  W "value "; W def.MLName; W " : Widget";
+let write_named_create_p w wname =
+  w "value create_named : Widget -> string -> option list -> Widget ;;\n";
+  w "             (* [create p name options] creates a new widget with ";
+  w "                parent p and new patch component name.";
+  w "		     Options are restricted to the widget class subset,";
+  w "		     and checked dynamically. *)\n"
+;;
+
+let write_command_p wname w def =
+  w "value "; w def.MLName; w " : Widget";
   begin match def.Arg with
     Unit -> ()       (* no arg *)
   | Product tyl -> 
-        do_list (function t -> W " -> "; W (ppMLtype t)) tyl
-  | t -> W " -> "; W (ppMLtype t)
+        do_list (function t -> w " -> "; w (ppMLtype t)) tyl
+  | t -> w " -> "; w (ppMLtype t)
   end;
-  W " -> ";
-  W (ppMLtype def.Result);
-  W " ;;\n";
-  W "               (* tk command: ."; W wname; W " "; W def.TkName; W " *)\n"
+  w " -> ";
+  w (ppMLtype def.Result);
+  w " ;;\n";
+  w "               (* tk command: ."; w wname; w " "; w def.TkName; w " *)\n"
 ;;
 
-let write_function_p W def =
-  W "value "; W def.MLName; W " : ";
+let write_function_p w def =
+  w "value "; w def.MLName; w " : ";
   begin match def.Arg with
     Product tyl -> 
-        do_list (function t -> W (ppMLtype t); W " -> ") tyl
-  | t -> W (ppMLtype t); W " -> "
+        do_list (function t -> w (ppMLtype t); w " -> ") tyl
+  | t -> w (ppMLtype t); w " -> "
   end;
-  W (ppMLtype def.Result);
-  W " ;;\n";
-  W "               (* tk function: "; W def.TkName; W " *)\n"
+  w (ppMLtype def.Result);
+  w " ;;\n";
+  w "               (* tk function: "; w def.TkName; w " *)\n"
 ;;
