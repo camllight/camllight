ead746d1f0a781c20ffa904779738388336c40c4
xleroy <xavier.leroy@inria.fr>
1994-11-14 17:41:11 +0000

    Alpha-conversion dans connect.c et bind.c pour faire plaisir a gcc.

--- sources/contrib/libunix/bind.c
+++ sources/contrib/libunix/bind.c
@@ -5,12 +5,12 @@
 
 #include "socketaddr.h"
   
-value unix_bind(sock, addr)      /* ML */
-     value sock, addr;
+value unix_bind(socket, address)      /* ML */
+     value socket, address;
 {
   int ret;
-  get_sockaddr(addr);
-  ret = bind(Int_val(sock), &sock_addr.s_gen, sock_addr_len);
+  get_sockaddr(address);
+  ret = bind(Int_val(socket), &sock_addr.s_gen, sock_addr_len);
   if (ret == -1) uerror("bind", Nothing);
   return Val_unit;
 }
