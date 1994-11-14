250053f068f30b86940d06cdd31cbb27146f50a5
rouaix <rouaix@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1994-11-14 13:27:01 +0000

    Fixed: quoted tk names

--- trunk/sources/contrib/camltk/compiler/compile.ml
+++ trunk/sources/contrib/camltk/compiler/compile.ml
@@ -1,5 +1,35 @@
 #open "tables";;
 
+(* should be shared  with support *)
+let cindex p s start = find start
+  where rec find i =
+    if i >= string_length s 
+    then raise Not_found
+    else if p (nth_char s i) then i 
+    else find (i+1) 
+;;
+let must_quote c = 
+    c == `[` or c == `]` or c == `$` or c == `{` or c == `}`
+;;
+
+let quote_string s =
+  let s = string_for_read s in
+  let rec sfr cur res =
+      try
+        let n = cindex must_quote s cur in
+        let repl = match nth_char s n with
+                    `[` -> "\["
+                  | `]` -> "\]"
+                  | `$` -> "\$" 
+                  | `{` -> "\{"
+                  | `}` -> "\}"
+                  |  c ->  failwith "subliminal" (* never happens *) in
+        let res' = res ^ (sub_string s cur (n-cur)) ^ repl in
+          sfr (succ n) res'
+      with Not_found ->
+        res ^ (sub_string s cur (string_length s - cur)) in
+  sfr 0 ""
+;;
 
 let catenate_sep sep =
   function 
@@ -28,57 +58,57 @@ let rec ppMLtype =
 
 
 (* Write an ML constructor *)
-let write_constructor W
+let write_constructor w
   {MLName = mlconstr; TkName = tkstring; Arg = argtyp} =
-     W mlconstr;
+     w mlconstr;
      begin match argtyp with
      (* if argument is unit, constructor has no argument *)
        	 Unit -> ()
-       | ty -> W " of "; W (ppMLtype ty)
+       | ty -> w " of "; w (ppMLtype ty)
      end
 ;;
 
 (* Write a rhs type decl *)
-let write_constructors W = function
+let write_constructors w = function
     [] -> failwith "empty type"
-  | x::l -> write_constructor W x;
+  | x::l -> write_constructor w x;
 	    do_list (function x ->
-		      W "\n\t| ";
-		      write_constructor W x)
+		      w "\n\t| ";
+		      write_constructor w x)
 		    l
 ;;
 
 (* List of constructors *)
-let write_constructor_set W sep = function
+let write_constructor_set w sep = function
     [] -> failwith "empty type"
-  | x::l -> W ("C" ^ x.MLName);
+  | x::l -> w ("C" ^ x.MLName);
       	    do_list (function x ->
-		       W sep;
-		       W ("C" ^ x.MLName))
+		       w sep;
+		       w ("C" ^ x.MLName))
                      l
 ;;
 
 (* Definition of a type *)	    
-let write_type W name typdef =
+let write_type w name typdef =
   (* The type itself *)
-  W ("type "^name^" =\n\t");
-  write_constructors W typdef.constructors;
-  W "\n;;\n\n";
+  w ("type "^name^" =\n\t");
+  write_constructors w typdef.constructors;
+  w "\n;;\n\n";
   (* Dynamic Subtyping *)
   if typdef.subtypes <> [] then begin
     (* The set of its constructors *)
-    W ("type "^name^"_constrs =\n\t");
-    write_constructor_set W "\n\t| " typdef.constructors;
-    W "\n;;\n\n";
+    w ("type "^name^"_constrs =\n\t");
+    write_constructor_set w "\n\t| " typdef.constructors;
+    w "\n;;\n\n";
     (* The set of all constructors *)
-    W ("let "^name^"_any_table = [");
-    write_constructor_set W "; " typdef.constructors;
-    W ("]\n;;\n");
+    w ("let "^name^"_any_table = [");
+    write_constructor_set w "; " typdef.constructors;
+    w ("]\n;;\n");
     (* The subset of constructors for each subtype *)
     do_list (function (s,l) ->
-      	       W ("let "^name^"_"^s^"_table = [");
-	       write_constructor_set W "; " l;
-	       W ("]\n;;\n"))
+      	       w ("let "^name^"_"^s^"_table = [");
+	       write_constructor_set w "; " l;
+	       w ("]\n;;\n"))
             typdef.subtypes
   end
 ;;
@@ -117,31 +147,31 @@ let rec converterTKtoCAML argname = function
  | _ -> failwith "bug"
 ;;
 
-let write_wrapper_code W fname argtys =
- W "(function () ->\n\t\t";
+let write_wrapper_code w fname argtys =
+ w "(function args ->\n\t\t";
  begin match argtys with
    Product tyl ->
      let vnames = varnames "a" (list_length tyl) in
      do_list2 (fun v ty ->
        	       	 let readf = match ty with
-		   String -> "(GetTkString !PipeTkCallB)"
-		 | List _ -> "(GetTkTokenList !PipeTkCallB)"
-		 | _ -> "(GetTkToken !PipeTkCallB)" in
-       	       	 W ("let "^v^" = "^(converterTKtoCAML readf ty) ^ " in\n\t\t")
+		   String -> "(arg_GetTkString args)"
+		 | List _ -> "(arg_GetTkTokenList args)"
+		 | _ -> "(arg_GetTkToken args)" in
+       	       	 w ("let "^v^" = "^(converterTKtoCAML readf ty) ^ " in\n\t\t")
 		 )
                vnames tyl;
-     W (fname^" "^(catenate_sep " " vnames))
+     w (fname^" "^(catenate_sep " " vnames))
  | List ty ->
-     W (fname ^"("^ (converterTKtoCAML "(GetTkTokenList !PipeTkCallB)" argtys)
+     w (fname ^"("^ (converterTKtoCAML "(arg_GetTkTokenList args)" argtys)
               ^")")
  | String ->
-     W (fname ^"("^ (converterTKtoCAML "(GetTkString !PipeTkCallB)" argtys)
+     w (fname ^"("^ (converterTKtoCAML "(arg_GetTkString args)" argtys)
               ^")")
  | ty ->
-     W (fname ^"("^ (converterTKtoCAML "(GetTkToken !PipeTkCallB)" ty)
+     w (fname ^"("^ (converterTKtoCAML "(arg_GetTkToken args)" ty)
               ^")")
  end;
- W ")"
+ w ")"
 ;;
 
 (*************************************************************)
@@ -192,30 +222,30 @@ let can_generate_parser constructors =
 
 
 (* we should avoid multiple walks *)
-let write_TKtoCAML W name typdef =
+let write_TKtoCAML w name typdef =
   match can_generate_parser typdef.constructors with
     NoParser ->    prerr_string ("You must write TKtoCAML" ^ name ^"\n")
   | ParserPieces pp -> begin
-      W ("let TKtoCAML"^name^" n =\n");
+      w ("let TKtoCAML"^name^" n =\n");
       (* First check integer *)
        if pp.intpar <> [] then begin
-      	 W ("   try " ^ (hd pp.intpar).MLName ^ "(int_of_string n)\n");
-         W ("   with _ ->\n")
+      	 w ("   try " ^ (hd pp.intpar).MLName ^ "(int_of_string n)\n");
+         w ("   with _ ->\n")
          end;
-       W ("\tmatch n with\n");
+       w ("\tmatch n with\n");
       let first = ref true in
        do_list (fun c -> 
-      	 if not !first then W "\t| " else W "\t";
+      	 if not !first then w "\t| " else w "\t";
 	 first := false;
-      	 W "\""; W c.TkName; W "\" -> "; W c.MLName; W "\n")
+      	 w "\""; w c.TkName; w "\" -> "; w c.MLName; w "\n")
 	 pp.zeroary ;
       let failcase = "raise (Invalid_argument \"TKtoCAML" ^ name ^ "\")"  in
       let final = if pp.stringpar <> [] then
             "n -> " ^ (hd pp.stringpar).MLName ^ " n"
          else " _ -> " ^ failcase   in
-      if not !first then W "\t| " else W "\t";
-      W final;
-      W "\n;;\n"
+      if not !first then w "\t| " else w "\t";
+      w final;
+      w "\n;;\n"
      end
 ;;
 
@@ -248,20 +278,20 @@ and converterCAMLtoTKp argnames = function
   | _ -> failwith "bug"
 ;;
 
-let write_clause W subtyp comp =
-  let Warrow () = 
-      W " -> ";
+let write_clause w subtyp comp =
+  let warrow () = 
+      w " -> ";
       if subtyp then 
-         W ("chk_sub \""^comp.MLName^"\" table C" ^ comp.MLName ^ "; ")
+         w ("chk_sub \""^comp.MLName^"\" table C" ^ comp.MLName ^ "; ")
   in
-  W comp.MLName;
+  w comp.MLName;
   match comp.Arg with
-    Unit -> Warrow (); W "\""; W comp.TkName; W "\""
+    Unit -> warrow (); w "\""; w (quote_string comp.TkName); w "\""
   | Product tyl ->
        let vars = varnames "a" (list_length tyl) in
-       	 W "( ";  W (catenate_sep ", " vars); W ")";
-	 Warrow(); W "\""; W comp.TkName; W "\"";
-	 do_list2 (fun v ty -> W "^\" \"^"; W (converterCAMLtoTK v ty))
+       	 w "( ";  w (catenate_sep ", " vars); w ")";
+	 warrow(); w "\""; w (quote_string comp.TkName); w "\"";
+	 do_list2 (fun v ty -> w "^\" \"^"; w (converterCAMLtoTK v ty))
 	          vars tyl
   | Function ty -> 
       let vars = match ty with
@@ -269,158 +299,162 @@ let write_clause W subtyp comp =
       | Unit -> []
       | ty -> ["p"]
       in (* TODO : wrapper code *)
-      W " f";
-      Warrow ();
-      W "\""; W comp.TkName; W "\"^\" \"^";
-      W  "let id = register_callback ";
+      w " f";
+      warrow ();
+      w "\""; w (quote_string comp.TkName); w "\"^\" \"^";
+      w  "let id = register_callback ";
       begin match ty with 
-      	   Unit ->  W "f"
-         | _ -> write_wrapper_code W "f" ty
+      	   Unit ->  w "(function _ -> f ())"
+         | _ -> write_wrapper_code w "f" ty
       end;
-      W (" in
-             \"[set name \"^id^\"; 
-                proc \"^id^\" { " ^(catenate_sep " " vars) ^ " } {
-                  global PipeTkCallB;
-	          puts $PipeTkCallB \" ^id^\"; ");
-      do_list (fun x -> W ("
-                  puts $PipeTkCallB $"^x^";"))
-	      vars;
-      W ("
-	          flush $PipeTkCallB }; set name ]\"")
+      w (" in \"{camlcb \"^id^\"}\"")
   | ty -> 
-      W " x"; Warrow();
+      w " x"; warrow();
       if comp.TkName <> "" then begin
-      	  W "\""; W comp.TkName; W "\"^\" \"^"
+      	  w "\""; w (quote_string comp.TkName); w "\"^\" \"^"
       end;
-      W (converterCAMLtoTK "x" ty)
+      w (converterCAMLtoTK "x" ty)
 ;;
 	 
-let write_CAMLtoTK W name typdef =
-  W ("let CAMLtoTK"^name);
+let write_CAMLtoTK w name typdef =
+  w ("let CAMLtoTK"^name);
   let subtyp = typdef.subtypes <> [] in
   if subtyp then 
-    W " table";
-  W(" = function\n\t");
-  write_clause W subtyp (hd typdef.constructors);
-  do_list (fun c -> W "\n\t| "; write_clause W subtyp c) (tl typdef.constructors);
-  W "\n;;\n\n"
+    w " table";
+  w(" = function\n\t");
+  write_clause w subtyp (hd typdef.constructors);
+  do_list (fun c -> w "\n\t| "; write_clause w subtyp c) (tl typdef.constructors);
+  w "\n;;\n\n"
 ;;
 
-let write_function_body W def names =
+let write_function_body w def names =
   (* Argument passing *)
   begin match def.Arg with
     Unit -> ()
   | List ty -> 
-    W ("\tdo_list (function x -> Send2Tk(" ^ converterCAMLtoTK "x" ty ^ ")) " 
+    w ("\tdo_list (function x -> Send2Tk buf (" ^ converterCAMLtoTK "x" ty ^ ")) " 
                  ^ hd names ^";\n")
   | Product tyl ->
       do_list2 (fun v ty ->
-      	          W("\tSend2Tk(" ^ converterCAMLtoTK v ty ^ ");\n"))
+      	          w("\tSend2Tk buf (" ^ converterCAMLtoTK v ty ^ ");\n"))
 	       names tyl
   | ty ->
-      W("\tSend2Tk(" ^ converterCAMLtoTK (hd names) ty ^ ");\n")
+      w("\tSend2Tk buf (" ^ converterCAMLtoTK (hd names) ty ^ ");\n")
   end;
   (* Closing *)
   begin match def.Result with
     Unit ->
-      W "\tSend2TkEval()\n"
+      w "\tSend2TkEval buf; ()\n"
   | List ty ->
-      W "\tSend2Tk \"]; flush $PipeTkResult\";\n";
-      W "\tSend2TkEval();\n";
-      W ("\tmap "^ converterTKtoCAML "(GetTkTokenList !PipeTkResult)" ty)
+      w "\tSend2Tk buf result_footer;\n";
+      w "\tlet res = Send2TkEval buf in\n";
+      w ("\tmap "^ converterTKtoCAML "(res_GetTkTokenList res)" ty)
   | Product tyl ->
-      W "\tSend2Tk \"]; flush $PipeTkResult\";\n";
-      W "\tSend2TkEval();\n";
+      w "\tSend2Tk buf result_footer;\n";
+      w "\tlet res = Send2TkEval buf in\n";
       let rnames = varnames "r" (list_length tyl) in
        do_list2 (fun r ty ->
-                  W ("\tlet " ^ r ^ " = ");
-                  W (converterTKtoCAML "(GetTkToken !PipeTkResult)" ty);
-       	       	  W (" in\n"))
+                  w ("\tlet " ^ r ^ " = ");
+                  w (converterTKtoCAML "(res_GetTkToken res)" ty);
+       	       	  w (" in\n"))
                 rnames
                 tyl;
-       W (catenate_sep "," rnames)
+       w (catenate_sep "," rnames)
   | String ->
-      W "\tSend2Tk \"]; flush $PipeTkResult\";\n";
-      W "\tSend2TkEval();\n";
-      W (converterTKtoCAML "(GetTkString !PipeTkResult)" String)
+      w "\tSend2Tk buf result_footer;\n";
+      w "\tlet res = Send2TkEval buf in\n";
+      w (converterTKtoCAML "(res_GetTkString res)" String)
   | ty ->
-      W "\tSend2Tk \"]; flush $PipeTkResult\";\n";
-      W "\tSend2TkEval();\n";
-      W "\t";
-      W (converterTKtoCAML "(GetTkToken !PipeTkResult)" ty)
+      w "\tSend2Tk buf result_footer;\n";
+      w "\tlet res = Send2TkEval buf in\n";
+      w "\t";
+      w (converterTKtoCAML "(res_GetTkToken res)" ty)
   end;
-  W (";;\n\n")
+  w (";;\n\n")
 ;;
 
-let write_command wclass W def =
-  W ("let "^def.MLName^" w ");
+let write_command wclass w def =
+  w ("let "^def.MLName^" w ");
   let names = 
     match def.Arg with
      Unit -> []
   | Product tyl -> varnames "a" (list_length tyl)
   | _ -> ["a"]
   in
-  (* Write arguments *)
+  (* write arguments *)
   begin match names with
-    [] -> W "=\n"
-  | l -> W (catenate_sep " " l); W " =\n"
+    [] -> w "=\n"
+  | l -> w (catenate_sep " " l); w " =\n"
   end;
   (* Beginning of command *)
-  W ("\tcheck_widget_class w \""^ wclass ^ "\";\n");
+  w ("\tcheck_widget_class w \""^ wclass ^ "\";\n");
   begin match def.Result with
     Unit -> 
-        W  "\tSend2TkStart \"$PipeTkCallB\";\n";
-        W ("\tSend2Tk(widget_name w);\n");
-      	W ("\tSend2Tk \""^def.TkName^"\";\n")
+        w  "\tlet buf = Send2TkStart false in \n";
+        w ("\tSend2Tk buf (widget_name w);\n");
+      	w ("\tSend2Tk buf \""^(quote_string def.TkName)^"\";\n")
   | String ->
-        W  "\tSend2TkStart \"$PipeTkResult\";\n";
-      	W ("\tSend2Tk \"nputs $PipeTkResult [\";\n");
-        W ("\tSend2Tk(widget_name w);\n");
-        W ("\tSend2Tk \""^def.TkName^"\";\n")
+        w  "\tlet buf = Send2TkStart true in \n";
+	w  "\tSend2Tk buf result_string_header;\n";
+        w ("\tSend2Tk buf (widget_name w);\n");
+        w ("\tSend2Tk buf \""^(quote_string def.TkName)^"\";\n")
   | _ -> 
-        W  "\tSend2TkStart \"$PipeTkResult\";\n";
-      	W ("\tSend2Tk \"puts $PipeTkResult [\";\n");
-        W ("\tSend2Tk(widget_name w);\n");
-        W ("\tSend2Tk \""^def.TkName^"\";\n")
+        w  "\tlet buf = Send2TkStart true in \n";
+	w  "\tSend2Tk buf result_header;\n";
+        w ("\tSend2Tk buf (widget_name w);\n");
+        w ("\tSend2Tk buf \""^(quote_string def.TkName)^"\";\n")
   end;
-  write_function_body W def names
+  write_function_body w def names
 ;;
 
-let write_function W def =
-  W ("let "^def.MLName^" ");
+let write_function w def =
+  w ("let "^def.MLName^" ");
   let names = 
     match def.Arg with
      Unit -> []
   | Product tyl -> varnames "a" (list_length tyl)
   | _ -> ["a"]
   in
-  (* Write arguments *)
+  (* write arguments *)
   begin match names with
-    [] -> W "() =\n"
-  | l -> W (catenate_sep " " l); W " =\n"
+    [] -> w "() =\n"
+  | l -> w (catenate_sep " " l); w " =\n"
   end;
   (* Beginning of command *)
   begin match def.Result with
     Unit -> 
-        W  "\tSend2TkStart \"$PipeTkCallB\";\n";
-      	W ("\tSend2Tk \""^def.TkName^"\";\n")
+        w  "\tlet buf = Send2TkStart false in \n";
+      	w ("\tSend2Tk buf \""^(quote_string def.TkName)^"\";\n")
   | String ->
-        W  "\tSend2TkStart \"$PipeTkResult\";\n";
-      	W ("\tSend2Tk \"nputs $PipeTkResult ["^def.TkName^"\";\n")
+        w  "\tlet buf = Send2TkStart true in \n";
+	w  "\tSend2Tk buf result_string_header;\n";
+	w ("\tSend2Tk buf \""^(quote_string def.TkName)^"\";\n")
   | _ -> 
-        W  "\tSend2TkStart \"$PipeTkResult\";\n";
-      	W ("\tSend2Tk \"puts $PipeTkResult ["^def.TkName^"\";\n")
+        w  "\tlet buf = Send2TkStart true in \n";
+	w  "\tSend2Tk buf result_header;\n";
+	w ("\tSend2Tk buf \""^(quote_string def.TkName)^"\";\n")
   end;
-  write_function_body W def names
+  write_function_body w def names
 ;;
 
-let write_create W class =
-  W  "let create parent options =\n";
-  W  "   Send2TkStart \"$PipeTkCallB\";\n";
-  W ("   Send2Tk \"" ^ class ^ "\";\n");
-  W ("   let w = new_widget_atom \"" ^ class ^ "\" parent in\n");
-  W ("      Send2Tk(widget_name w);\n");
-  W ("      Send2Tk(" ^ converterCAMLtoTK "options" (List(Subtype("option",class))) ^ ");\n");
-  W ("      Send2TkEval();\n");
-  W ("      w\n;;\n")
+let write_create w class =
+  w  "let create parent options =\n";
+  w  "   let buf = Send2TkStart true in\n";
+  w ("   Send2Tk buf \"" ^ class ^ "\";\n");
+  w ("   let w = new_widget_atom \"" ^ class ^ "\" parent in\n");
+  w ("      Send2Tk buf (widget_name w);\n");
+  w ("      Send2Tk buf (" ^ converterCAMLtoTK "options" (List(Subtype("option",class))) ^ ");\n");
+  w ("      Send2TkEval buf;\n");
+  w ("      w\n;;\n")
+;;
+
+let write_named_create w class =
+  w  "let create_named parent name options =\n";
+  w  "   let buf = Send2TkStart true in\n";
+  w ("   Send2Tk buf \"" ^ class ^ "\";\n");
+  w ("   let w = new_named_widget \"" ^ class ^ "\" parent name in\n");
+  w ("      Send2Tk buf (widget_name w);\n");
+  w ("      Send2Tk buf (" ^ converterCAMLtoTK "options" (List(Subtype("option",class))) ^ ");\n");
+  w ("      Send2TkEval buf;\n");
+  w ("      w\n;;\n")
 ;;
