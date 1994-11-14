8a4874a2a869310fc971e86a21233e19d437bca0
rouaix <rouaix@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1994-11-14 14:09:48 +0000

    Enhanced for documentation

--- trunk/sources/contrib/camltk/libsupport/support.mli
+++ trunk/sources/contrib/camltk/libsupport/support.mli
@@ -1,39 +1,57 @@
-(* Widget is an abstract type *)
-type Widget;;
+(* Support for widget manipulations *)
 
+type Widget
+      	(* Widget is an abstract type *)
+;;
 
-value default_toplevel_widget : Widget;;
-       (* The default toplevel widget is ".", of type "toplevel" *)
+value default_toplevel_widget : Widget
+      	(* The default toplevel widget is ".", of type "toplevel" *)
+;;
 
-value new_toplevel_widget : string -> Widget;;
+value new_toplevel_widget : string -> Widget
        (* the argument must be a valid window name: no 8bit char, start
           with a lowercase, ... *)
+;;
 
-(* The following functions are used internally.        *)
-(* There is normally no need for them in users programs *)
+(* The following functions are used internally.
+   There is normally no need for them in users programs *)
           
-value widget_name : Widget -> string;;
+value widget_name : Widget -> string
+      (* Return the name (tk "path") of a widget *)
+;;
+value new_widget_atom : string -> Widget -> Widget
+and   new_named_widget : string -> Widget -> string -> Widget
+      (* Abstract creation functions *)
+;;
 
-value CAMLtoTKWidget : 'a -> Widget -> string;;
-value TKtoCAMLWidget : string -> Widget;;
+value CAMLtoTKWidget : 'a -> Widget -> string
+and   TKtoCAMLWidget : string -> Widget
+      (* Conversion functions *)
+;;
 
-value Widget_any_table : string list;;
-value Widget_menu_table : string list;;
-value Widget_frame_table : string list;;
-value new_widget_atom : string -> Widget -> Widget;;
+value Widget_any_table : string list
+and Widget_menu_table : string list
+and Widget_frame_table : string list
+and chk_sub : string -> 'a list -> 'a -> unit
+and check_widget_class : Widget -> string -> unit
+      (* Widget subtyping *)
+;;
 exception IllegalWidgetType of string
+      (* Raised when widget command applied illegally*)
 ;;
-value check_widget_class : Widget -> string -> unit;;
 
-value chk_sub : string -> 'a list -> 'a -> unit;;
-
-value quote_string : string -> string;;
-type symbol
+value quote_string : string -> string
+and catenate_sep : string -> string list -> string
+and split_str : (char -> bool) -> string -> string list
+      (* Various string manipulations *)
 ;;
-value CAMLtoTKsymbol : symbol -> string;;
-value TKtoCAMLsymbol : string -> symbol;;
 
-value catenate_sep : string -> string list -> string
+
+
+type symbol == string
+      (* A simpler "string" type *)
 ;;
-value split_str : (char -> bool) -> string -> string list
+
+value CAMLtoTKsymbol : symbol -> string
+and   TKtoCAMLsymbol : string -> symbol
 ;;
