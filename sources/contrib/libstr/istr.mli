bd31ae56bf367ff3fe807fcbfac01fb0ee44e6f9
xleroy <xavier.leroy@inria.fr>
1994-11-14 16:40:03 +0000

    Ajout des sources.

---
+++ sources/contrib/libstr/istr.mli
@@ -0,0 +1,10 @@
+(* Internals of the str library. *)
+
+#open "str";;
+
+value compile_regexp: string -> bool -> regexp = 2 "str_compile_regexp"
+  and beginning_group: int -> int = 1 "str_beginning_group"
+  and end_group: int -> int = 1 "str_end_group"
+  and remains_args: unit -> bool = 1 "str_remains_args"
+  and replacement_text: string -> string -> string = 2 "str_replacement_text"
+;;
