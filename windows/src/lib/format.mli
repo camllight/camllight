2b1e7377d032a5f8a34c98b9bc83c6419e89d9c4
weis <pierre.weis@inria.fr>
1994-11-23 15:29:52 +0000

    Ajout de stack__peek format utilise stack.ml et queue.ml

--- windows/src/lib/format.mli
+++ windows/src/lib/format.mli
@@ -1,8 +1,9 @@
 (* Pretty print *)
 
 (* This module implements a pretty-printing facility to format text
-   within boxes; the pretty-printer breaks lines using specified break
-   hints. *) 
+   within ``pretty-printing boxes''; the pretty-printer breaks lines
+   using specified break hints. The behaviour of pretty-printing
+   commands is unspecified if there is no opened pretty-printing box. *)
 
 #open "io";;
 
@@ -12,19 +13,28 @@ value open_vbox : int -> unit;;
            with offset [d]. 
            This box is ``vertical'': every break hint inside this
            box leads to a new line.
+<<<<<<< format.mli
+           Every new line in this box is indented [my_number]
+           more than the indentation of the box. *)
+=======
            Every new line in the box is indented [d] more than the
            indentation of the box. *)
+>>>>>>> 1.2
 value open_hbox : unit -> unit;;
         (* [open_hbox ()] opens a new pretty-printing box.
-           This box is ``horizontal'': no new line occurs in
-           this box. *) 
+           This box is ``horizontal'': no new line occurs in this box
+           (new lines may occur inside boxes nested deeper). *)
 value open_hvbox : int -> unit;;
         (* [open_hovbox d] opens a new pretty-printing box
            with offset [d]. 
            This box is ``horizontal-vertical'': it behaves as an
            ``horizontal'' box if it fits on a single line,
            otherwise it behaves as a ``vertical'' one.
+<<<<<<< format.mli
+           Every new line in this box is indented [my_number] more than the
+=======
            Every new line in the box is indented [d] more than the
+>>>>>>> 1.2
            indentation of the box. *)
 value open_hovbox : int -> unit;;
         (* [open_hovbox d] opens a new pretty-printing box
@@ -32,7 +42,11 @@ value open_hovbox : int -> unit;;
            This box is ``horizontal or vertical'': break hints
            inside this box may lead to a new line, if there is no more room
            on the line to print the rest of the box.
+<<<<<<< format.mli
+           Every new line in this box is indented [my_number] more than the
+=======
            Every new line in the box is indented [d] more than the
+>>>>>>> 1.2
            indentation of the box. *)
 value close_box : unit -> unit;;
         (* Close the most recently opened pretty-printing box. *)
@@ -55,12 +69,21 @@ value print_bool : bool -> unit;;
 
 (* Breaking hints *)
 value print_break : int * int -> unit;;
+<<<<<<< format.mli
+        (* Indicate a break hint in the current pretty-printing box.
+           [print_break (spaces, offset)] indicates that the line may
+           be broken at this point, if the pretty-printer cannot
+           display the content of the current box on the line.
+           If a new line occurs, [offset] is added to
+           the current indentation, otherwise [spaces] spaces are printed. *)
+=======
         (* Break hint in a pretty-printing box.
            [print_break (nspaces, offset)] indicates that the line may
            be split (a newline character is printed) at this point,
            if the contents of the current box do not fit on one line.
            If the line is split at that point, [offset] is added to
            the current indentation, otherwise [nspaces] spaces are printed. *)
+>>>>>>> 1.2
 value print_cut : unit -> unit;;
         (* [print_cut ()] is equivalent to [print_break (0,0)].
            This allows line splitting at the current point, without printing
@@ -89,29 +112,58 @@ value close_tbox : unit -> unit;;
 
 (* Tabulation breaks *)
 value print_tbreak : int * int -> unit;;
+<<<<<<< format.mli
+        (* Indicates a break hint in the current tabulation box.
+           [print_tbreak (spaces, offset)] moves insertion point to
+=======
         (* Break hint in a tabulation box.
            [print_tbreak (spaces, offset)] moves the insertion point to
+>>>>>>> 1.2
            the next tabulation ([spaces] being added to this position).
+<<<<<<< format.mli
+           Nothing occurs if insertion point is already on a
+           tabulation mark.
+           If there is no next tabulation on the line then a new line
+           occurs and insertion point moves to the left-most tabulation of
+           the box.
+           If a new line occurs [offset] is added to the current
+=======
            If there is no next tabulation on the line, then a newline
            is printed and the insertion point moves to
            the first tabulation of the box.
            If a newline is printed, [offset] is added to the current
+>>>>>>> 1.2
            indentation. *)
 value set_tab : unit -> unit;;
-        (* Set a tabulation after the last displayed character. *)
+        (* Set a tabulation mark at the current insertion point. *)
 value print_tab : unit -> unit;;
         (* [print_tab ()] is equivalent to [print_tbreak (0,0)]. *)
 
 (* Margin *)
 value set_margin : int -> unit;;
+<<<<<<< format.mli
+        (* [set_margin my_number] sets the value of the right margin
+           to [my_number] (count in characters): this
+           value guides the detection of line overflows that leads to
+           line break decisions.
+           Nothing happens if [my_number] is not greater than 1. *)
+=======
         (* Set the position of the right margin (in characters). *)
+>>>>>>> 1.2
 value get_margin : unit -> int;;
         (* Return the position of the right margin. *)
 
 (* Maximum indentation limit *)
 value set_max_indent : int -> unit;;
+<<<<<<< format.mli
+        (* [set_max_indent my_number] sets the value of the maximum
+           indentation limit to [my_number] (in characters):
+           boxes opened further are rejected to the left.
+           Nothing happens if [my_number] is not greater than 1. *)
+=======
         (* Set the value of the maximum indentation limit (in characters):
            once this limit is reached, boxes are rejected to the left. *)
+>>>>>>> 1.2
 value get_max_indent : unit -> int;;
         (* Return the value of the maximum indentation limit (in
            characters). *)
@@ -122,7 +174,8 @@ value set_max_print_depth : int -> unit;;
            of boxes simultaneously opened.
            Material inside boxes nested deeper is printed as an
            ellipsis (more precisely the text returned by
-           [get_ellipsis_text]). *)
+           [get_ellipsis_text]).
+           Nothing happens if [my_number] is not greater than 1. *)
 value get_max_print_depth : unit -> int;;
         (* Return the maximum number of boxes allowed before ellipsis. *)
 
--- windows/src/lib/stack.ml
+++ windows/src/lib/stack.ml
@@ -1,19 +1,27 @@
 #open "list";;
 #open "exc";;
+#open "eq";;
+#open "int";;
 
 type 'a t = { mutable c : 'a list };;
 
 let new () = { c = [] };;
 
+let clear s = s.c <- [];;
+
 let push x s = s.c <- x :: s.c;;
+
+let rec item n = function
+    [] -> raise Empty
+  | x::l -> if n = 0 then x else item (n-1) l;;
+
+let peek s n = item n s.c;;
 
 let pop s =
   match s.c with
     hd::tl -> s.c <- tl; hd
   | []     -> raise Empty
 ;;
-
-let clear s = s.c <- [];;
 
 let length s = list_length s.c;;
 
