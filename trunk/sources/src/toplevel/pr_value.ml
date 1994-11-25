ee69931a0b118048c5eb4b324a14b1ed32de13e4
weis <weis@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1994-11-25 12:47:38 +0000

    Modifs de pr_value pour avoir une meilleure ellipse.

--- trunk/sources/src/toplevel/pr_value.ml
+++ trunk/sources/src/toplevel/pr_value.ml
@@ -75,7 +75,7 @@ let cautious f arg = try f arg with Ellipsis -> print_string "...";;
 let rec print_val prio depth obj ty =
   if depth < 0 then raise Ellipsis;
   try
-    (find_printer ty) obj; ()
+    find_printer ty obj; ()
   with Not_found ->
     match (type_repr ty).typ_desc with
       Tvar _ ->
@@ -83,11 +83,13 @@ let rec print_val prio depth obj ty =
     | Tarrow(ty1, ty2) ->
         print_string "<fun>"
     | Tproduct(ty_list) ->
-        if prio > 0 then print_string "(";
+        if prio > 0 then begin open_hovbox 1; print_string "(" end
+         else open_hovbox 0;
         print_val_list 1 depth obj ty_list;
-        if prio > 0 then print_string ")"
+        if prio > 0 then print_string ")";
+        close_box()
     | Tconstr({info = {ty_abbr = Tabbrev(params, body)}}, ty_list) ->
-        cautious (print_val prio depth obj) (expand_abbrev params body ty_list)
+        print_val prio depth obj (expand_abbrev params body ty_list)
     | Tconstr(cstr, [ty_arg]) when same_type_constr cstr constr_type_list ->
         print_list depth obj ty_arg
     | Tconstr(cstr, [ty_arg]) when same_type_constr cstr constr_type_vect ->
@@ -129,7 +131,7 @@ and print_concrete_type prio depth obj cstr ty ty_list =
             print_space();
             open_hovbox 1;
             print_string "(";
-            print_val_list 1 (depth - 1) obj (filter_product n ty_arg);
+            print_val_list 1 depth obj (filter_product n ty_arg);
             print_string ")";
             close_box();
             if prio > 1 then print_string ")";
@@ -154,42 +156,44 @@ and print_concrete_type prio depth obj cstr ty ty_list =
         with Unify ->
           fatal_error "print_val: types should match"
         end;
-        print_val 0 depth (obj_field obj lbl.info.lbl_pos) ty_arg;
+        print_val 0 (depth - 1) (obj_field obj lbl.info.lbl_pos) ty_arg;
         close_box() in
-      let rec print_fields depth = function
-        [] -> ()
-      | [lbl] -> print_field depth lbl
-      | lbl::rest ->
-          print_field depth lbl; print_string ";"; print_space();
-          print_fields depth rest
+      let print_fields depth label_list =
+          let rec loop depth b = function
+              [] -> ()
+            | lbl :: rest ->
+                if b then begin print_string ";"; print_space() end;
+                print_field depth lbl;
+                loop (depth - 1) true rest in
+          loop depth false label_list
       in
       open_hovbox 1;
       print_string "{";
-      cautious (print_fields (depth - 1)) label_list;
+      cautious (print_fields depth) label_list;
       print_string "}";
       close_box()
   | Abbrev_type(_,_) ->
       fatal_error "print_val: abbrev type"
 
 and print_val_list prio depth obj ty_list =
-  let rec print_list depth i = function
-    [] ->
-      fatal_error "print_val_list"
-  | [ty] ->
-       print_val prio depth (obj_field obj i) ty
-  | ty::ty_list ->
-       print_val prio depth (obj_field obj i) ty;
-       print_string ","; print_space();
-       print_list depth (succ i) ty_list
-  in cautious (print_list (depth - 1) 0) ty_list
+  let print_list depth i =
+      let rec loop depth i = function
+          [] -> ()
+        | ty :: ty_list ->
+           if i > 0 then begin print_string ","; print_space() end;
+           print_val prio (depth - 1) (obj_field obj i) ty;
+           loop (depth - 1) (succ i) ty_list in
+      loop depth 0
+  in cautious (print_list depth 0) ty_list
 
 and print_list depth obj ty_arg =
   let rec print_conses depth cons =
    if obj_tag cons != 0 then begin
      print_val 0 depth (obj_field cons 0) ty_arg;
-     if obj_tag (obj_field cons 1) != 0 then begin
+     let next_obj = obj_field cons 1 in
+     if obj_tag next_obj != 0 then begin
        print_string ";"; print_space();
-       print_conses (depth - 1) (obj_field cons 1)
+       print_conses (depth - 1) next_obj
      end
    end
  in
@@ -200,11 +204,15 @@ and print_list depth obj ty_arg =
    close_box()
 
 and print_vect depth obj ty_arg =
-   let print_items depth obj =
-      for i = 0 to obj_size obj - 1 do
-       if i > 0 then print_string ";"; print_space();
-       print_val depth 0 (obj_field obj i) ty_arg
-      done
+  let print_items depth obj =
+      let rec loop depth i =
+          if i < obj_size obj then
+           begin
+            if i > 0 then begin print_string ";"; print_space() end;
+            print_val 0 (depth - 1) (obj_field obj i) ty_arg;
+            loop (depth - 1) (i + 1)
+           end in
+      loop depth 0
   in
     open_hovbox 2;
     print_string "[|";
@@ -215,5 +223,4 @@ and print_vect depth obj ty_arg =
 
 let print_value obj ty = print_val 0 !printer_depth obj ty
 ;;
-
 
