854eed3220c74a0886e4c5ff1d8d36249102fea4
xleroy <xavier.leroy@inria.fr>
1994-12-14 09:47:34 +0000

    Ajout d'une table de hash pour ne lire le fichier de messages qu'une fois.

--- windows/src/compiler/interntl.ml
+++ windows/src/compiler/interntl.ml
@@ -4,56 +4,74 @@
 
 let language = ref "";;
 
-let translate msg =
-  if string_length !language == 0 then msg else begin
-    try
-      let msgfile = find_in_path "camlmsgs.txt" in
-      let ic = open_in msgfile in
-      let tag_buffer = create_string 16
-      and msg_buffer = create_string 1024 in
-      let rec store_tag c i =
-        if i >= 16 then i else (tag_buffer.[i] <- c; succ i)
-      and store_msg c i =
-        if i >= 1024 then i else (msg_buffer.[i] <- c; succ i)
-      and read_line i =
-        match input_char ic with
-          `\n` -> i
-        | `\\` -> begin match input_char ic with
-                    `\\` -> read_line(store_msg `\\` i)
-                  | `n`  -> read_line(store_msg `\n` i)
-                  | `\n` -> skip_blanks i
-                  | c    -> read_line(store_msg c (store_msg `\\` i))
-                  end
-        | c    -> read_line(store_msg c i)
-      and skip_blanks i =
-        match input_char ic with
-          ` `|`\t` -> skip_blanks i
-        | c        -> read_line(store_msg c i)
-      and read_tag i =
-        match input_char ic with
-          `:`           -> (i, skip_blanks 0)
-        | ` `|`\n`|`\t` -> read_tag i
-        | c             -> read_tag(store_tag c i)
-      and find_translation () =
-        let (tag_len, msg_len) = read_tag 0 in
-        let tag = sub_string tag_buffer 0 tag_len in
-        if tag = !language then sub_string msg_buffer 0 msg_len
-        else if tag = "src" then msg (* No translation in the right language *)
-        else find_translation()
-      and find_message () =
-        let (tag_len, msg_len) = read_tag 0 in
-        if sub_string tag_buffer 0 tag_len = "src"
-         & sub_string msg_buffer 0 msg_len = msg
-        then find_translation()
-        else find_message()
-      in
-      try
-        let transl = find_message() in close_in ic; transl
-      with End_of_file -> (* Message is not in message file *)
-        close_in ic; msg
-    with Cannot_find_file _ ->
+let read_transl_file msgfile =
+  let ic = open_in msgfile in
+  let tag_buffer = create_string 16
+  and msg_buffer = create_string 1024 in
+  let rec store_tag c i =
+    if i >= 16 then i else (tag_buffer.[i] <- c; succ i)
+  and store_msg c i =
+    if i >= 1024 then i else (msg_buffer.[i] <- c; succ i)
+  and read_line i =
+    match input_char ic with
+      `\n` -> i
+    | `\\` -> begin match input_char ic with
+                `\\` -> read_line(store_msg `\\` i)
+              | `n`  -> read_line(store_msg `\n` i)
+              | `\n` -> skip_blanks i
+              | c    -> read_line(store_msg c (store_msg `\\` i))
+              end
+    | c    -> read_line(store_msg c i)
+  and skip_blanks i =
+    match input_char ic with
+      ` `|`\t` -> skip_blanks i
+    | c        -> read_line(store_msg c i)
+  and read_tag i =
+    match input_char ic with
+      `:`           -> (i, skip_blanks 0)
+    | ` `|`\n`|`\t` -> read_tag i
+    | c             -> read_tag(store_tag c i) in
+  let transl_tbl = hashtbl__new 59 in
+  let currsrc = ref "" in
+  begin try
+    while true do
+      let (tag_len, msg_len) = read_tag 0 in
+      if sub_string tag_buffer 0 tag_len = "src" then
+        currsrc := sub_string msg_buffer 0 msg_len
+      else if sub_string tag_buffer 0 tag_len = !language then
+        hashtbl__add transl_tbl !currsrc (sub_string msg_buffer 0 msg_len)
+      else ()
+    done
+  with End_of_file -> (* Message is not in message file *)
+    close_in ic
+  end;
+  transl_tbl
+;;
+
+type translation_table =
+    Unknown
+  | None
+  | Transl of (string, string) hashtbl__t;;
+
+let transl_table = ref Unknown;;
+
+let rec translate msg =
+  match !transl_table with
+    None ->
       msg
-  end
+  | Transl tbl ->
+      begin try hashtbl__find tbl msg with Not_found -> msg end
+  | Unknown ->
+      transl_table :=
+        if string_length !language == 0 then
+          Unknown
+        else begin
+          try
+            Transl(read_transl_file(find_in_path "camlmsgs.txt"))
+          with Cannot_find_file _ | sys__Sys_error _ | sys__Break ->
+            Unknown
+        end;
+      translate msg
 ;;
 
 let fprintf oc (fmt : ('a, out_channel, unit) printf__format) =
