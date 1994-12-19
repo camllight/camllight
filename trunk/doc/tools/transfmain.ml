3213cf9f0e9f32f64fd7ccc889de4fb968241d38
xleroy <xleroy@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1994-12-19 12:33:17 +0000

    \mysection -> \section.

--- trunk/doc/tools/transfmain.ml
+++ trunk/doc/tools/transfmain.ml
@@ -1,4 +1,8 @@
-#open "transf";;
-#open "lexing";;
+let main() =
+  let lexbuf = lexing__create_lexer_channel std_in in
+  if vect_length sys__command_line >= 2 & sys__command_line.(1) = "-html"
+  then htmltransf__main lexbuf
+  else transf__main lexbuf;
+  exit 0;;
 
-main (create_lexer_channel std_in); exit 0;;
+printexc__f main ();;
