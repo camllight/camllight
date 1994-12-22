e415bbeb57a19f3dddda42eb2a7de83a98bb336a
doligez <doligez@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1994-12-22 14:48:38 +0000

    Documentation de gc.mli
    Changement d'algo et ajout d'une fonction d'initalisation serieuse dans random.

--- trunk/windows/src/lib/random.mli
+++ trunk/windows/src/lib/random.mli
@@ -3,11 +3,15 @@
 value init : int -> unit;;
   (* Initialize the generator, using the argument as a seed.
      The same seed will always yield the same sequence of numbers. *)
+value full_init : int vect -> unit;;
+  (* Same as [init] but takes more data as seed.  It is not
+     useful to give more than 55 integers. *)
 
 value int : int -> int
   (* [random__int bound] returns a random number between 0 (inclusive)
-     and [bound] (exclusive).  [bound] must be smaller than $2^{30}$. *)
+     and [bound] (exclusive).  [bound] must be positive and smaller
+     than $2^{30}$. *)
 and float : float -> float
-  (* [random__float] returns a random number between 0 (inclusive)
+  (* [random__float bound] returns a random number between 0 (inclusive)
      and [bound] (exclusive). *)
 ;;
