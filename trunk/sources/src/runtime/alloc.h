b0fc1614fe5f79108e220833b6247fd71d71ba7e
xleroy <xleroy@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1994-12-30 10:30:09 +0000

    Rectification des prototypes.

--- trunk/sources/src/runtime/alloc.h
+++ trunk/sources/src/runtime/alloc.h
@@ -2,29 +2,18 @@
 #define _alloc_
 
 
+#include "misc.h"
 #include "mlvalues.h"
 
-#ifdef ANSI
-extern value alloc(mlsize_t, tag_t);
-extern value alloc_tuple(mlsize_t);
-extern value alloc_string(mlsize_t);
-extern value alloc_final(mlsize_t, final_fun, mlsize_t, mlsize_t);
-extern value copy_string(char *);
-extern value copy_string_array(char **);
-extern value copy_double(double);
-extern value alloc_array(value (*funct)(value), char ** array);
-extern int convert_flag_list(value, int *);
-#else
-value alloc();
-value alloc_tuple();
-value alloc_string();
-value alloc_final();
-value copy_string();
-value copy_string_array();
-value copy_double();
-value alloc_array();
-int convert_flag_list();
-#endif
+value alloc P((mlsize_t, tag_t));
+value alloc_tuple P((mlsize_t));
+value alloc_string P((mlsize_t));
+value alloc_final P((mlsize_t, final_fun, mlsize_t, mlsize_t));
+value copy_string P((char *));
+value copy_string_array P((char **));
+value copy_double P((double));
+value alloc_array P((value (*funct) P((value)), char ** array));
+int convert_flag_list P((value, int *));
 
 
 #endif /* _alloc_ */
