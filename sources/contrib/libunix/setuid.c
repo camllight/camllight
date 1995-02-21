edb8c7bd3002cf49516e3d4643195b11cc0f10b0
nobody <nobody@nowhere.com>
1995-02-21 14:57:32 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.18.2'.

--- sources/contrib/libunix/setuid.c
+++
@@ -1,9 +0,0 @@
-#include <mlvalues.h>
-#include "unix.h"
-
-value unix_setuid(uid)           /* ML */
-     value uid;
-{
-  if (setuid(Int_val(uid)) == -1) uerror("setuid", Nothing);
-  return Val_unit;
-}
--- sources/examples/kb/loadall.ml
+++
@@ -1,6 +0,0 @@
-load "prelude.ml";;
-load "terms.ml";;
-load "equation.ml";;
-load "order.ml";;
-load "kb.ml";;
-load "go.ml";;
