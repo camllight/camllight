d898b1c50f3162d233b9126e049e376bdda14096
nobody <nobody@nowhere.com>
1995-03-09 17:59:38 +0000

    This commit was manufactured by cvs2svn to create branch 'DOS'.

--- sources/contrib/libnum/test/end_test.ml
+++
@@ -1,8 +0,0 @@
-#open "test";;
-
-print_newline();;
-if !error_occurred then begin
-  prerr_endline "************* TEST FAILED ****************"; exit 2
-end else
-  exit 0
-;;
--- sources/contrib/libunix/close.c
+++
@@ -1,9 +0,0 @@
-#include <mlvalues.h>
-#include "unix.h"
-
-value unix_close(fd)             /* ML */
-     value fd;
-{
-  if (close(Int_val(fd)) == -1) uerror("close", Nothing);
-  return Val_unit;
-}
