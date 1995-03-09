799fd6d3576144d8e1e7b3521a9553f09274ce80
nobody <nobody@nowhere.com>
1995-03-09 16:40:25 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.36.2'.

--- sources/contrib/libunix/geteuid.c
+++
@@ -1,7 +0,0 @@
-#include <mlvalues.h>
-#include "unix.h"
-
-value unix_geteuid()             /* ML */
-{
-  return Val_int(geteuid());
-}
--- sources/contrib/libunix/getgid.c
+++
@@ -1,7 +0,0 @@
-#include <mlvalues.h>
-#include "unix.h"
-
-value unix_getgid()              /* ML */
-{
-  return Val_int(getgid());
-}
