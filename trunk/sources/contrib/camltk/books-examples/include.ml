51fe70d1f7eb96ca8bc093c6f187a99793d99a6f
rouaix <rouaix@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1995-04-24 13:38:04 +0000

    Additions and cleanup.

--- trunk/sources/contrib/camltk/books-examples/include.ml
+++ trunk/sources/contrib/camltk/books-examples/include.ml
@@ -19,4 +19,6 @@ include"ouster-f16.12";;
 include"ouster-f16.13";;
 include"ouster-f16.14";;
 include"ouster-f16.15";;
-(* include"ouster-f16.16";; *)
+include"ouster-f17.3";;
+include"ouster-f17.4";;
+include"ouster-f17.5";;
