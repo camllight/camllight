f2852511b0385a0d6d4a880a3b6308d821584801
nobody <nobody@nowhere.com>
1995-05-03 09:10:50 +0000

    This commit was manufactured by cvs2svn to create tag 'DOS_release7b4'.

--- sources/contrib/camlsearch/source/directory_and_file.mli
+++
@@ -1,6 +0,0 @@
-
-value directory_content : string -> string list;;
-
-value is_directory : string -> bool;;
-
-value file_exist : string -> bool;;
--- sources/contrib/debugger/pattern_matching.mli
+++
@@ -1,8 +0,0 @@
-(************************ Simple pattern matching ***************)
-
-#open "value";;
-#open "globals";;
-#open "parser_aux";;
-
-value pattern_matching :
-  PATTERN -> VALUE -> typ -> (string * VALUE * typ) list;;
