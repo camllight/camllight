bd8e1d5c2863d804847b918a5dc59451436ea400
nobody <nobody@nowhere.com>
1995-05-19 13:37:01 +0000

    This commit was manufactured by cvs2svn to create tag 'tcleval'.

--- sources/examples/picomach/stockage.mli
+++
@@ -1,9 +0,0 @@
-#open "code";;
-
-exception Erreur of string;;
-
-value initialise: unit -> unit
-  and assemble: instruction -> unit
-  and poser_étiquette: string -> unit
-  and valeur_étiquette: string -> int
-  and extraire_code: unit -> instruction vect;;
--- windows/examples/showsort/loadall.ml
+++
@@ -1,3 +0,0 @@
-load "showsort";;
-#open "showsort";;
-print_string "To run: animate();;"; print_newline();;
