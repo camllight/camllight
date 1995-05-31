36ca50aa8c1aa1c4bc6a4e185903297f04e73c57
rouaix <rouaix@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1995-05-31 14:21:24 +0000

    Fixed textvariable name

--- trunk/sources/contrib/camltk/books-examples/ouster-f16.3.ml
+++ trunk/sources/contrib/camltk/books-examples/ouster-f16.3.ml
@@ -1,13 +1,9 @@
 #open "tk";;
 
-(* a bit weird in Camltk *)
-let textvariable_name =  textvariable__CAMLtoTKTextVariable
-;;
-
 let top = OpenTk () in
  let watch tv =
      let t = toplevelw__create (support__new_toplevel_widget "watch") [] in
-     let l = label__create t [Text ("Value of "^textvariable_name tv^" :")]
+     let l = label__create t [Text ("Value of "^textvariable__name tv^" :")]
      and v = label__create t [TextVariable tv] in
        pack [l;v] [Side Side_Left] in
 
