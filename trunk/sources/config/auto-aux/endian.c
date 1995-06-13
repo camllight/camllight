3dde28a517c97e7342370d108ec84c3cf131dd6c
xleroy <xleroy@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1995-06-13 13:01:07 +0000

    Remplacement switch par #ifdef pour eviter des warnings sur les machines 32 bits.

--- trunk/sources/config/auto-aux/endian.c
+++ trunk/sources/config/auto-aux/endian.c
@@ -1,22 +1,19 @@
+#include "m.h"
+
 main()
 {
   long n[2];
   char * p, * bigendian, * littleendian;
 
-  switch (sizeof(long)) {
-  case 4:
+#ifndef SIXTYFOUR
     n[0] = 0x41424344L;
     bigendian = "ABCD";
     littleendian = "DCBA";
-    break;
-  case 8:
+#else
     n[0] = 0x4142434445464748L;
     bigendian = "ABCDEFGH";
     littleendian = "HGFEDCBA";
-    break;
-  default:
-    exit(2);
-  }
+#endif
   n[1] = 0;
   p = (char *) n;
   if (strcmp(p, bigendian) == 0)
