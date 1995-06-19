73a27e1654c72b73a31a6333375bacc1c5f8d45a
weis <pierre.weis@inria.fr>
1995-06-19 16:18:09 +0000

    Ajout option -W lors des compilations. Renommage des variables en majuscules.

--- sources/examples/kb/equation.ml
+++ sources/examples/kb/equation.ml
@@ -7,11 +7,11 @@ type rule == int * (int * (term * term));;
 
 (* standardizes an equation so its variables are 1,2,... *)
 
-let mk_rule M N =
-  let all_vars = union (vars M) (vars N) in
+let mk_rule m n =
+  let all_vars = union (vars m) (vars n) in
   let (k,subst) =
     it_list (fun (i,sigma) v -> (i+1,(v,Var(i))::sigma)) (1,[]) all_vars in
-  (k-1, (substitute subst M, substitute subst N))
+  (k-1, (substitute subst m, substitute subst n))
 ;;
 
 (* checks that rules are numbered in sequence and returns their number *)
@@ -20,9 +20,9 @@ let (check_rules: rule list -> int) =
                           else failwith "Rule numbers not in sequence") 0
 ;;
 
-let pretty_rule (k,(n,(M,N)): rule) =
+let pretty_rule (k,(_,(m,n)): rule) =
   print_int k; print_string " : ";
-  pretty_term M; print_string " = "; pretty_term N;
+  pretty_term m; print_string " = "; pretty_term n;
   print_newline()
 ;;
 
@@ -31,34 +31,34 @@ let pretty_rules = do_list pretty_rule
 
 (****************** Rewriting **************************)
 
-(* Top-level rewriting. Let eq:L=R be an equation, M be a term such that L<=M.
-   With sigma = matching L M, we define the image of M by eq as sigma(R) *)
-let reduce L M =
-  substitute (matching L M)
+(* Top-level rewriting. Let eq:l=r be an equation, m be a term such that l<=m.
+   With sigma = matching l m, we define the image of m by eq as sigma(r) *)
+let reduce l m =
+  substitute (matching l m)
 ;;
 
-(* A more efficient version of can (rewrite1 (L,R)) for R arbitrary *)
-let reducible L = redrec
-  where rec redrec M =
+(* A more efficient version of can (rewrite1 (l,r)) for r arbitrary *)
+let reducible l = redrec
+  where rec redrec m =
     try
-      matching L M; true
+      matching l m; true
     with Failure _ ->
-      match M with Term(_,sons) -> exists redrec sons
+      match m with Term(_,sons) -> exists redrec sons
                 |         _     -> false
 ;;
 
 (* mreduce : rules -> term -> term *)
-let mreduce rules M =
-  let redex (_,(_,(L,R))) = reduce L M R in try_find redex rules
+let mreduce rules m =
+  let redex (_,(_,(l,r))) = reduce l m r in try_find redex rules
 ;;
 
 (* One step of rewriting in leftmost-outermost strategy, with multiple rules *)
 (* fails if no redex is found *)
 (* mrewrite1 : rules -> term -> term *)
 let mrewrite1 rules = rewrec
-  where rec rewrec M =
+  where rec rewrec m =
     try
-      mreduce rules M
+      mreduce rules m
     with Failure _ ->
       let rec tryrec = function
           [] -> failwith "mrewrite1"
@@ -67,23 +67,23 @@ let mrewrite1 rules = rewrec
               rewrec son :: rest
             with Failure _ ->
               son :: tryrec rest in
-      match M with
+      match m with
           Term(f, sons) -> Term(f, tryrec sons)
         | _ -> failwith "mrewrite1"
 ;;
 
 (* Iterating rewrite1. Returns a normal form. May loop forever *)
 (* mrewrite_all : rules -> term -> term *)
-let mrewrite_all rules M = rew_loop M
-  where rec rew_loop M =
+let mrewrite_all rules m = rew_loop m
+  where rec rew_loop m =
     try
-      rew_loop(mrewrite1 rules M)
+      rew_loop(mrewrite1 rules m)
     with Failure _ ->
-      M
+      m
 ;;
 
 (*
-pretty_term (mrewrite_all Group_rules M where M,_=<<A*(I(B)*B)>>);;
+pretty_term (mrewrite_all Group_rules m where m,_=<<A*(I(B)*B)>>);;
 ==> A*U
 *)
 
