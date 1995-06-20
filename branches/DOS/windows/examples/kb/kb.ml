e19828fce86c4835629913b8e7fd3032d1b8721c
xleroy <xleroy@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1995-06-20 09:07:19 +0000

    MAJ Makefile. MAJ variables en majuscules.

--- branches/DOS/windows/examples/kb/kb.ml
+++ branches/DOS/windows/examples/kb/kb.ml
@@ -4,16 +4,16 @@
 
 (****************** Critical pairs *********************)
 
-(* All (u,sig) such that N/u (&var) unifies with M,
+(* All (u,sig) such that n/u (&var) unifies with m,
    with principal unifier sig *)
 (* super : term -> term -> (num list & subst) list *)
-let super M = suprec where rec suprec = function
-    Term(_,sons) as N ->
+let super m = suprec where rec suprec = function
+    Term(_,sons) as n ->
       let collate (pairs,n) son =
        (pairs @ map (fun (u,sig) -> (n::u,sig)) (suprec son), n+1) in
       let insides = fst (it_list collate ([],1) sons) in
         begin try
-          ([], unify(M,N)) :: insides
+          ([], unify(m,n)) :: insides
         with Failure _ ->
           insides
         end
@@ -21,35 +21,35 @@ let super M = suprec where rec suprec = function
 ;;
 
 (* Ex :
-let (M,_) = <<F(A,B)>> 
-and (N,_) = <<H(F(A,x),F(x,y))>> in super M N;;
+let (m,_) = <<F(A,B)>> 
+and (n,_) = <<H(F(A,x),F(x,y))>> in super m n;;
 ==> [[1],[2,Term ("B",[])];                      x <- B
      [2],[2,Term ("A",[]); 1,Term ("B",[])]]     x <- A  y <- B
 *)
 
-(* All (u,sig), u&[], such that N/u unifies with M *)
+(* All (u,sig), u&[], such that n/u unifies with m *)
 (* super_strict : term -> term -> (num list & subst) list *)
-let super_strict M = function
+let super_strict m = function
       Term(_,sons) ->
         let collate (pairs,n) son =
-          (pairs @ map (fun (u,sig) -> (n::u,sig)) (super M son), n+1) in
+          (pairs @ map (fun (u,sig) -> (n::u,sig)) (super m son), n+1) in
         fst (it_list collate ([],1) sons)
     | _ -> []
 ;;
 
-(* Critical pairs of L1=R1 with L2=R2 *)
+(* Critical pairs of l1=r1 with l2=r2 *)
 (* critical_pairs : term_pair -> term_pair -> term_pair list *)
-let critical_pairs (L1,R1) (L2,R2) =
+let critical_pairs (l1,r1) (l2,r2) =
   let mk_pair (u,sig) =
-     substitute sig (replace L2 u R1), substitute sig R2 in
-  map mk_pair (super L1 L2);;
+     substitute sig (replace l2 u r1), substitute sig r2 in
+  map mk_pair (super l1 l2);;
 
-(* Strict critical pairs of L1=R1 with L2=R2 *)
+(* Strict critical pairs of l1=r1 with l2=r2 *)
 (* strict_critical_pairs : term_pair -> term_pair -> term_pair list *)
-let strict_critical_pairs (L1,R1) (L2,R2) =
+let strict_critical_pairs (l1,r1) (l2,r2) =
   let mk_pair (u,sig) =
-    substitute sig (replace L2 u R1), substitute sig R2 in
-  map mk_pair (super_strict L1 L2)
+    substitute sig (replace l2 u r1), substitute sig r2 in
+  map mk_pair (super_strict l1 l2)
 ;;
 
 (* All critical pairs of eq1 with eq2 *)
@@ -72,13 +72,13 @@ let deletion_message (k,_) =
 ;;
 
 (* Generate failure message *)
-let non_orientable (M,N) =
-    pretty_term M; print_string " = "; pretty_term N; print_newline()
+let non_orientable (m,n) =
+    pretty_term m; print_string " = "; pretty_term n; print_newline()
 ;;
 
 (* Improved Knuth-Bendix completion procedure *)
 
-let kb_completion greater = kbrec where rec kbrec n rules =
+let kb_completion greater = kbrec where rec kbrec rnum rules =
   let normal_form = mrewrite_all rules
   and get_rule k = assoc k rules in process
   where rec process failures = processf
@@ -87,30 +87,31 @@ let kb_completion greater = kbrec where rec kbrec n rules =
      match eqs with
      [] ->
       if k<l then next_criticals (k+1,l) else
-      if l<n then next_criticals (1,l+1) else
+      if l<rnum then next_criticals (1,l+1) else
        (match failures with
           [] -> rules (* successful completion *)
         | _  -> message "Non-orientable equations :";
                 do_list non_orientable failures;
                 failwith "kb_completion")
-   | (M,N)::eqs ->
-      let M' = normal_form M
-      and N' = normal_form N
+   | (m,n)::eqs ->
+      let m' = normal_form m
+      and n' = normal_form n
       and enter_rule(left,right) =
-        let new_rule = (n+1, mk_rule left right) in
+        let new_rule = (rnum+1, mk_rule left right) in
           pretty_rule new_rule;
-          let left_reducible (_,(_,(L,_))) = reducible left L in
+          let left_reducible (_,(_,(l,_))) = reducible left l in
           let redl,irredl = partition left_reducible rules in
             do_list deletion_message redl;
             let irreds = (map right_reduce irredl
-                 where right_reduce (m,(_,(L,R))) = 
-                       m,mk_rule L (mrewrite_all (new_rule::rules) R))
+                 where right_reduce (m,(_,(l,r))) = 
+                       m,mk_rule l (mrewrite_all (new_rule::rules) r))
             and eqs' = map (fun (_,(_,pair)) -> pair) redl in
-             kbrec (n+1) (new_rule::irreds) [] (k,l) (eqs @ eqs' @ failures) in
-      if M'=N' then processkl eqs else
-      if greater(M',N') then enter_rule(M',N') else
-      if greater(N',M') then enter_rule(N',M') else
-        process ((M',N')::failures) (k,l) eqs)
+             kbrec (rnum+1) (new_rule::irreds) [] (k,l) (eqs @ eqs' @ failures)
+      in
+      if m'=n' then processkl eqs else
+      if greater(m',n') then enter_rule(m',n') else
+      if greater(n',m') then enter_rule(n',m') else
+        process ((m',n')::failures) (k,l) eqs)
   and next_criticals (k,l) =
     try
       let (v,el) = get_rule l in
