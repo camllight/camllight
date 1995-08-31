a545797987a5ceeed2f19c097d8e9d3e6e4f62d6
rouaix <francois.rouaix@inria.fr>
1995-08-31 11:54:24 +0000

    Unification index

--- sources/contrib/camltk4/libsupport/builtin_text.ml
+++ sources/contrib/camltk4/libsupport/builtin_text.ml
@@ -42,59 +42,16 @@ let ppTextModifier = function
  | WordEnd -> " wordend"
 ;;
 
-type BaseTextIndex =
-   TI_LineChar of int * int		(* tk keyword: l.c *)
- | TI_Mark of TextMark			(* tk keyword: *)
- | TI_End 				(* tk keyword: end *)
- | TI_At of int * int			(* tk keyword: @x,y *)
- | TI_TagFirst of TextTag		(* tk keyword: tag.first *)
- | TI_TagLast of TextTag		(* tk keyword: tag.last *)
- | TI_Embedded of Widget                (* tk keyword: a widget name *)
-;;
-
-let ppBaseTextIndex = function
-   TI_LineChar (l,c) -> (string_of_int l) ^ "." ^ (string_of_int c)
- | TI_Mark s -> s
- | TI_End -> "end"
- | TI_At(x,y) -> "@" ^ (string_of_int x) ^ "," ^ (string_of_int y)
- | TI_TagFirst s -> s ^ ".first"
- | TI_TagLast s -> s ^ ".last"
- | TI_Embedded w -> widget_name w
-;;
-
-let CAMLtoTKBaseTextIndex i =
-  TkToken (ppBaseTextIndex i)
-;;
-
-let char_index c s = find 0
-  where rec find i =
-    if i >= string_length s 
-    then raise Not_found
-    else if nth_char s i = c then i 
-    else find (i+1) 
-;;
-
-
-(* Assumes only l.c is returned *)
-let TKtoCAMLBaseTextIndex s = 
-  try
-   let p = char_index `.` s in
-     TI_LineChar(int_of_string (sub_string s 0 p), 
-      	         int_of_string (sub_string s (p+1) (string_length s - p - 1)))
-  with 
-    Not_found -> raise (Invalid_argument ("TKtoCAMLBaseTextIndex: "^s))
-;;
-
-
 type TextIndex =
-   TextIndex of BaseTextIndex * TextModifier list
+   TextIndex of Index * TextModifier list
  | TextIndexNone
 ;;
 
 let ppTextIndex = function
    TextIndexNone -> ""
  | TextIndex (base, ml) -> 
-     it_list (prefix ^) (ppBaseTextIndex base) (map ppTextModifier ml)
+     let (TkToken ppbase) = CAMLtoTKIndex Index_text_table base in 
+       it_list (prefix ^) ppbase (map ppTextModifier ml)
 ;;
 
 let CAMLtoTKTextIndex i = 
