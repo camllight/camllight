eb3887d1ccd91ea0d64f76578ae6e6211cede68d
rouaix <francois.rouaix@inria.fr>
1995-08-31 11:54:05 +0000

    Simplification des noms.

--- sources/contrib/camltk4/libsupport/builtin_palette.ml
+++ sources/contrib/camltk4/libsupport/builtin_palette.ml
@@ -1,13 +1,13 @@
 type PaletteType =
-  | NumberOfGrayShades of int
-  | NumberOfRGBShades of int * int * int
+  | GrayShades of int
+  | RGBShades of int * int * int
 ;;
 
 let CAMLtoTKPaletteType = function
-    NumberOfGrayShades (foo) -> TkToken (string_of_int foo)
-  | NumberOfRGBShades (r,v,b) -> TkToken ((string_of_int r)^"/"^
-                                         (string_of_int v)^"/"^
-                                         (string_of_int b))
+    GrayShades (foo) -> TkToken (string_of_int foo)
+  | RGBShades (r,v,b) -> TkToken (string_of_int r^"/"^
+				  string_of_int v^"/"^
+				  string_of_int b)
 ;;
 
 
