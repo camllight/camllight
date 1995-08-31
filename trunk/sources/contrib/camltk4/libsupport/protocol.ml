4349999094ebfedb94fa88d50e96ddfb8c19760f
rouaix <rouaix@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1995-08-31 11:56:32 +0000

    Added: hashtblc (without space leak) Added: timers

--- trunk/sources/contrib/camltk4/libsupport/protocol.ml
+++ trunk/sources/contrib/camltk4/libsupport/protocol.ml
@@ -47,11 +47,11 @@ let TkEval args =
 
 (* Large initial size to avoid leaks due to growing hashtbl algorithm *)
 let callback_naming_table = 
-   (hashtbl__new 401 : (string, callback_buffer -> unit) hashtbl__t) 
+   (hashtblc__new 401 : (string, callback_buffer -> unit) hashtblc__t) 
 ;;
 
 let callback_memo_table =
-   (hashtbl__new 401 : (Widget, string) hashtbl__t)
+   (hashtblc__new 401 : (Widget, string) hashtblc__t)
 ;;
 
 let new_function_id =
@@ -65,21 +65,21 @@ let new_function_id =
 (* The callback should be cleared when w is destroyed *)
 let register_callback w f =
   let id = new_function_id () in
-    hashtbl__add callback_naming_table id f;
-    hashtbl__add callback_memo_table w id;
+    hashtblc__add callback_naming_table id f;
+    hashtblc__add callback_memo_table w id;
     id
 ;;
 
 let clear_callback id =
-  hashtbl__remove callback_naming_table id
+  hashtblc__remove callback_naming_table id
 ;;
 
 (* Clear callbacks associated to a given widget *)
 let remove_callbacks w =
-  let cb_ids = hashtbl__find_all callback_memo_table w in
+  let cb_ids = hashtblc__find_all callback_memo_table w in
     do_list clear_callback cb_ids;
     for i = 1 to list_length cb_ids do
-      hashtbl__remove callback_memo_table w
+      hashtblc__remove callback_memo_table w
     done
 ;;
 
@@ -92,7 +92,7 @@ let install_cleanup () =
       	   remove_callbacks w;
 	   remove_widget w
     | _ -> raise (TkError "bad cleanup callback") in
-  hashtbl__add callback_naming_table "0" wrapped_remove;
+  hashtblc__add callback_naming_table "0" wrapped_remove;
   (* setup general destroy callback *)
   tcl_eval "bind all <Destroy> {camlcb 0 %W}"
 ;;
@@ -107,7 +107,11 @@ let dispatch_callback = function
       prerr_string "\n";
       flush std_err
       end;
-    (hashtbl__find callback_naming_table id) args
+    (hashtblc__find callback_naming_table id) args;
+    if !debug then begin
+      prerr_string "<<-\n";
+      flush std_err
+    end
 ;;
 
 (* Different version of initialisation functions *)
@@ -144,6 +148,10 @@ let MainLoop =
 (* Extensions *)
 let add_fileinput fd f =
   let id = register_callback dummy_widget (function _ -> f ()) in
+    if !debug then begin
+      prerr_string "fileinput:"; prerr_string id; prerr_string "\n";
+      flush stderr
+    end;
     add_file_input fd id
 ;;
 
@@ -151,4 +159,22 @@ let remove_fileinput =
     rem_file_input
 ;;
 
+type Timer == (TkTimer * string)
+;;
+(* A timer is used only once, so we must clean the callback table *)
+let add_timer milli f =
+  let id = new_function_id () in
+  let wrapped _ =
+    clear_callback id; (* do it first in case f raises exception *)
+    f() in
+  let t = internal_add_timer milli id in
+  hashtblc__add callback_naming_table id wrapped;
+  t,id
+;;
 
+(* If the timer has never been used, there is a small space leak in
+   the C heap, where a copy of id has been stored *)
+let remove_timer (tkTimer, id) =
+  internal_rem_timer tkTimer;
+  clear_callback id
+;;
