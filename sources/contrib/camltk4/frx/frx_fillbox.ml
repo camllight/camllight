7a3935e4f078a6904e1d8f7294381e4a9fbd4700
rouaix <francois.rouaix@inria.fr>
1995-09-14 13:20:25 +0000

    bugs, colors

--- sources/contrib/camltk4/frx/frx_fillbox.ml
+++ sources/contrib/camltk4/frx/frx_fillbox.ml
@@ -3,24 +3,29 @@
 (*
  * Progress indicators
  *)
+let okcolor = NamedColor "#3cb371"
+and kocolor = NamedColor "#dc5c5c"
+;;
+
 
 let new_vertical parent w h =
   let f = frame__create parent 
       	    [Width (Pixels w); Height (Pixels h); 
-      	     Relief Sunken; BorderWidth (Millimeters 1.0)] in
-  let initfill = [Width (Pixels w); Height (Pixels 0); Background Green]
+      	     Relief Sunken; BorderWidth (Pixels 1)] in
+  let initfill = [Width (Pixels w); Height (Pixels 0); Background okcolor]
   and initempty = [Width (Pixels w); Height (Pixels h)] in
   let ffill = frame__create f initfill
   and fempty = frame__create f initempty in
      pack[ffill][Side Side_Bottom; Fill Fill_X];
      pack[fempty][Side Side_Top; Fill Fill_Both];
 
-  f, (function percent ->
-      	match percent with
+  f, (function
 	   0 -> frame__configure ffill initfill;
 	        frame__configure fempty initempty
-         | -1 -> frame__configure ffill [Background Red]
-         | _ -> let hf = percent*h/100 in
+         | -1 -> frame__configure ffill [Background kocolor]
+         | n ->
+	    let percent = if n > 100 then 95 else n in
+      	    let hf = percent*h/100 in
 		  frame__configure ffill [Height (Pixels hf)];
 		  frame__configure fempty [Height (Pixels (h - hf))])
 ;;
@@ -28,21 +33,22 @@ let new_vertical parent w h =
 let new_horizontal parent w h =
   let f = frame__create parent 
       	    [Width (Pixels w); Height (Pixels h); 
-      	     Relief Sunken; BorderWidth (Millimeters 1.0)] in
-  let initfill = [Width (Pixels 0); Height (Pixels h); Background Green]
+      	     Relief Sunken; BorderWidth (Pixels 1)] in
+  let initfill = [Width (Pixels 0); Height (Pixels h); Background okcolor]
   and initempty = [Width (Pixels w); Height (Pixels h)] in
   let ffill = frame__create f initfill
   and fempty = frame__create f initempty in
      pack[ffill][Side Side_Left; Fill Fill_Y];
      pack[fempty][Side Side_Right; Fill Fill_Both];
 
-  f, (function percent ->
-      	match percent with
+  f, (function
            0 -> frame__configure ffill initfill;
 	        frame__configure fempty initempty
-	 | -1 -> frame__configure ffill [Background Green]
-         | _ -> let hf = percent*h/100 in
-		  frame__configure ffill [Width (Pixels hf)];
-		  frame__configure fempty [Width (Pixels (h - hf))])
+	 | -1 -> frame__configure ffill [Background kocolor]
+         | n ->
+	    let percent = if n > 100 then 95 else n in
+            let wf = percent*w/100 in
+		  frame__configure ffill [Width (Pixels wf)];
+		  frame__configure fempty [Width (Pixels (w - wf))])
 ;;
 
