0756a6b130e35525105fb86a840283184ba1c8d3
xleroy <xleroy@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1996-12-05 10:51:41 +0000

    concat -> string__concat, format -> printf__sprintf.

--- trunk/sources/contrib/libstr/str.ml
+++ trunk/sources/contrib/libstr/str.ml
@@ -8,14 +8,7 @@ let first_chars s n = sub_string s 0 n;;
 
 let last_chars s n = sub_string s (string_length s - n) n;;
 
-let concat sl =
-  let len = it_list (fun l s -> l + string_length s) 0 sl in
-  let res = create_string len in
-  it_list (fun pos s ->
-             let l = string_length s in
-             blit_string s 0 res pos l; pos+l)
-          0 sl;
-  res;;
+let concat = string__concat;;
 
 let join sep = function
     [] -> ""
@@ -101,91 +94,3 @@ let bounded_split expr text num =
 
 let split expr text = bounded_split expr text 0;;
 
-#open "obj";;
-
-let format format =
-  let format = (magic format : string) in
-  let res = ref [] in
-
-  let rec doprn start i =
-    if i >= string_length format then begin
-      if i > start then res := sub_string format start (i-start) :: !res;
-      magic(concat(rev !res))
-    end else
-      if nth_char format i != `%` then
-        doprn start (i+1)
-      else begin
-        if i > start then res := sub_string format start (i-start) :: !res;
-        let j = skip_args (succ i) in
-        match nth_char format j with
-          `%` ->
-            doprn j (succ j)
-        | `s` ->
-            magic(dostring i j)
-        | `c` ->
-            magic(fun c ->
-                res := make_string 1 c :: !res;
-                doprn (succ j) (succ j))
-        | `d` | `i` | `o` | `x` | `X` | `u` ->
-            magic(doint i j)
-        | `f` | `e` | `E` | `g` | `G` ->
-            magic(dofloat i j)
-        | `b` ->
-            magic(fun b ->
-                res := string_of_bool b :: !res;
-                doprn (succ j) (succ j))
-        | `a` ->
-            magic(fun printer arg ->
-              res := printer () arg :: !res;
-              doprn (succ j) (succ j))
-        | `t` ->
-            magic(fun printer ->
-              res := printer () :: !res;
-              doprn (succ j) (succ j))
-        | c ->
-            invalid_arg ("format: unknown format " ^ char_for_read c)
-      end
-
-  and skip_args j =
-    match nth_char format j with
-      `0` | `1` | `2` | `3` | `4` | `5` | `6` | `7` | `8` | `9` |
-      ` ` | `.` | `-` ->
-        skip_args (succ j)
-    | c ->
-        j
-    
-  and dostring i j s =
-    if j <= i+1 then
-      res := s :: !res
-    else begin
-      let p =
-        try
-          int_of_string (sub_string format (i+1) (j-i-1))
-        with _ ->
-          invalid_arg "format: bad %s format" in
-      if p > 0 & string_length s < p then begin
-        res := make_string (p - string_length s) ` ` :: !res;
-        res := s :: !res
-      end else if p < 0 & string_length s < -p then begin
-        res := s :: !res;
-        res := make_string (-p - string_length s) ` ` :: !res
-      end else
-        res := s :: !res
-    end;
-    doprn (succ j) (succ j)
-
-  and doint i j n =
-    let len = j-i in
-    let fmt = create_string (len+2) in
-    blit_string format i fmt 0 len;
-    set_nth_char fmt len `l`;
-    set_nth_char fmt (len+1) (nth_char format j);
-    res := format_int fmt n :: !res;
-    doprn (succ j) (succ j)
-
-  and dofloat i j f =
-    res := format_float (sub_string format i (j-i+1)) f :: !res;
-    doprn (succ j) (succ j)
-
-  in doprn 0 0
-;;
