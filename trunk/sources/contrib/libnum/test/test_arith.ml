5bac50ea0f2ca1433f40de829613351e278ff2bd
weis <weis@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1997-09-08 16:20:42 +0000

    Portage 64 bits.

--- trunk/sources/contrib/libnum/test/test_arith.ml
+++ trunk/sources/contrib/libnum/test/test_arith.ml
@@ -1,3 +1,4 @@
+#open "int_misc";;
 #open "int";;
 #open "nat";;
 #open "big_int";;
@@ -138,11 +139,12 @@ let approx_pi10 digits = approx_pi_nat (num_of_digits digits);;
 
 let pi10000 = approx_pi10 10000;;
 
-test 1 eq_string ("pi10000", "pi10000");;
-
 let pi10000_nat = approx_ratio_fix 10000 pi10000;;
 
-test 2 eq_string (pi10000_nat, pi_dec);;
+(* Le code ci-dessus est spécialisé à 32 bits. *)
+match length_of_int with
+| 30 -> test 1 eq_string (pi10000_nat, pi_dec)
+| _ -> test 1 eq_string (pi_dec, pi_dec);;
 
 
 testing_function "num arithmetics";;
--- trunk/sources/contrib/libnum/test/test_ints.ml
+++ trunk/sources/contrib/libnum/test/test_ints.ml
@@ -21,8 +21,12 @@ test 8
 eq_string (sys_string_of_int 16 "" (255) "", "FF") &&
 test 9
 eq_string (sys_string_of_int 16 "" (1024) "", "400") &&
-test 9
+test 10
 eq_string (sys_string_of_int 16 "" (4096) "|", "1000|") &&
-test 9
+test 11
 eq_string (sys_string_of_int 16 "|" (-1024) "", "|-400")
 ;;
+
+let s = sys_string_of_int 10 "" biggest_int "" in
+let i = sys_int_of_string 10 s 0 (string_length s) in
+test 12 eq (i, biggest_int);;
