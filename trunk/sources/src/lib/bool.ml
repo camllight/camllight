9d7c0b66ce1ec6d6b88c45fdd9117ccef71dddf4
weis <weis@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
1998-12-23 19:00:54 +0000

    Nouvelles fonction bool_of_string.

--- trunk/sources/src/lib/bool.ml
+++ trunk/sources/src/lib/bool.ml
@@ -1,1 +1,7 @@
-let string_of_bool = function false -> "false" | _ -> "true";;
+#open "exc";;
+
+let string_of_bool = function | false -> "false" | _ -> "true";;
+let bool_of_string = function
+  | "false" -> false
+  | "true" -> true
+  | _ -> raise (Invalid_argument "bool_of_string");;
