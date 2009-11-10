3ee98af4f874b6b3fe040e3282488427d6d54027
weis <weis@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
2009-11-10 18:17:23 +0000

    Include time.h to get the proper profile for time.

--- trunk/sources/contrib/libunix/time.c
+++ trunk/sources/contrib/libunix/time.c
@@ -1,9 +1,8 @@
 #include <mlvalues.h>
 #include "unix.h"
-
-extern long time();
+#include "time.h"
 
 value unix_time()                /* ML */
 {
-  return Val_long(time((long *) NULL));
+  return Val_long(time(NULL));
 }
