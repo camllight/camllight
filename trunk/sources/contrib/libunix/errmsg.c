86b1cdddb618e9a5ffbf804ce2f4023ef2f971a7
boisson <boisson@7b9ff45d-b53f-472c-83d8-1c28b3c590e6>
2009-11-13 07:23:08 +0000

    Remplacement sys_errlist par strerror dans libunix

--- trunk/sources/contrib/libunix/errmsg.c
+++ trunk/sources/contrib/libunix/errmsg.c
@@ -1,8 +1,12 @@
 #include <mlvalues.h>
 #include <alloc.h>
 
+#ifndef HAS_STRERROR
 extern int sys_nerr;
 extern char *sys_errlist[];
+#else
+extern char *strerror();
+#endif
 
 extern int error_table[];
 
@@ -11,9 +15,17 @@ value unix_error_message(err)
 {
   int errnum;
   errnum = error_table[Tag_val(err)];
+#ifndef HAS_STRERROR
   if (errnum < 0 || errnum >= sys_nerr) {
-    return copy_string("Unknown error");
+      return copy_string("Unknown error");
   } else {
-    return copy_string(sys_errlist[errnum]);
+      return copy_string(sys_errlist[errnum]);
   }
+#else
+  if (errnum < 0) {
+      return copy_string("Unknown error");
+  } else {
+      return copy_string(strerror(errnum));
+  }
+#endif
 }
